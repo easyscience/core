6014a9864ed7495eaaab27e42d7c3626e81822ba
Christian Vedel <christian.vedel@ess.eu>
2025-05-05 15:13:37 +0200

    Push changes to lowercase in filenames

---
+++ src/easyscience/utils/Exceptions.py
@@ -0,0 +1,11 @@
+#  SPDX-FileCopyrightText: 2023 EasyScience contributors  <core@easyscience.software>
+#  SPDX-License-Identifier: BSD-3-Clause
+#  © 2021-2023 Contributors to the EasyScience project <https://github.com/easyScience/EasyScience
+
+
+class CoreError(Exception):
+    pass
+
+
+class CoreSetException(CoreError):
+    pass
