10c76bcd0c550008498f99e1fa5f73b89c45886d
Christian Vedel <christian.vedel@ess.eu>
2025-05-07 16:30:20 +0200

    Reorganize imports

--- src/easyscience/base_classes/__init__.py
+++ src/easyscience/base_classes/__init__.py
@@ -1,9 +1,9 @@
+from .based_base import BasedBase
 from .collection_base import CollectionBase
 from .obj_base import ObjBase
-from .based_base import BasedBase
 
 __all__ = [
-    ObjBase,
     BasedBase,
     CollectionBase,
+    ObjBase,
 ]
