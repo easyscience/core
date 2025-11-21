6687c3392ac96c764bb9397aaad2d4c2737e6efc
Christian Vedel <christian.vedel@ess.eu>
2025-11-21 12:54:57 +0100

    Sort imports

--- src/easyscience/base_classes/model_base.py
+++ src/easyscience/base_classes/model_base.py
@@ -4,10 +4,12 @@ from __future__ import annotations
 #  SPDX-License-Identifier: BSD-3-Clause
 #  © 2021-2025 Contributors to the EasyScience project <https://github.com/easyScience/EasyScience
 from typing import TYPE_CHECKING
-from typing import Any
-from typing import Dict
-from typing import List
-from typing import Optional
+
+if TYPE_CHECKING:
+    from typing import Any
+    from typing import Dict
+    from typing import List
+    from typing import Optional
 
 from ..io import SerializerBase
 from ..variable import DescriptorNumber
