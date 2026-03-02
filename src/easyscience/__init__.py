f3d335003b0e9b81ac6b9c32b893243a9ededc23
Andrew Sazonov <andrew.sazonov@icloud.com>
2026-03-02 11:35:49 +0100

    Add back package init. Switch to importlib version

--- src/easyscience/__init__.py
+++ src/easyscience/__init__.py
@@ -1,3 +1,28 @@
-# SPDX-FileCopyrightText: 2021-2026 EasyScience contributors <https://github.com/easyscience>
-# SPDX-License-Identifier: BSD-3-Clause
-"""EasyScience library."""
+from importlib.metadata import version
+
+from .global_object import GlobalObject
+
+# Must be executed before any other imports
+global_object = GlobalObject()
+global_object.instantiate_stack()
+global_object.stack.enabled = False
+
+
+from .base_classes import ObjBase  # noqa: E402
+from .fitting import AvailableMinimizers  # noqa: E402
+from .fitting import Fitter  # noqa: E402
+from .variable import DescriptorNumber  # noqa: E402
+from .variable import Parameter  # noqa: E402
+
+
+__version__ = version('easyscience')
+
+__all__ = [
+    __version__,
+    global_object,
+    ObjBase,
+    AvailableMinimizers,
+    Fitter,
+    DescriptorNumber,
+    Parameter,
+]
