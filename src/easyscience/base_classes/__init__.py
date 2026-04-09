3dbc23a93e4e1cdb46a0dd81d105c0f1d8f855b9
rozyczko <piotr.rozyczko@gmail.com>
2026-04-09 15:22:29 +0200

    removed unneeded files

--- src/easyscience/base_classes/__init__.py
+++ src/easyscience/base_classes/__init__.py
@@ -3,18 +3,9 @@
 
 from .based_base import BasedBase
 from .collection_base import CollectionBase
-from .collection_base_easylist import CollectionBaseEasyList
 from .easy_list import EasyList
 from .model_base import ModelBase
 from .new_base import NewBase
 from .obj_base import ObjBase
 
-__all__ = [
-    BasedBase,
-    CollectionBase,
-    CollectionBaseEasyList,
-    ObjBase,
-    ModelBase,
-    NewBase,
-    EasyList,
-]
+__all__ = [BasedBase, CollectionBase, ObjBase, ModelBase, NewBase, EasyList]
